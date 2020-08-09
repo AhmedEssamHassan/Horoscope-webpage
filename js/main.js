83166acf2b5cea551fe7955da6d79387bfb5aaec
ahmed essam <ahmedessamsakr22@gmail.com>
2020-08-09 17:36:25 +0200

    adding main nav and animated borders for nav links

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* $(document).ready(function () {
+  $(".nav-item").mouseenter(function () {
+    console.log("kjdsfh");
+    $(".border-1").animate({
+      width: "100%"
+    },900,)
+  });
+});
+ */
