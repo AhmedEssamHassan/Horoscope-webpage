ee8780c9e075468211088896757c7d391bdb9c72
ahmed essam <ahmedessamsakr22@gmail.com>
2020-08-11 19:45:40 +0200

    adding main nav bar

--- js/main.js
+++
@@ -1,9 +0,0 @@
-/* $(document).ready(function () {
-  $(".nav-item").mouseenter(function () {
-    console.log("kjdsfh");
-    $(".border-1").animate({
-      width: "100%"
-    },900,)
-  });
-});
- */
